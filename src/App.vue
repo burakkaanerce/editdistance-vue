f1f395d99933848c2085a7f31f23e35236250535
burakkaanerce <burak.ercedogan@gmail.com>
2019-03-16 03:24:19 +0300

    modules system is ready

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Distance Finder</router-link> |
+      <router-link to="/about">Spell Checker</router-link>
+    </div>
+    <div v-if="alert.message" :class="`alert ${alert.type}`">
+      {{ alert.message }}
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -28,4 +34,57 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+.alert {
+  position: relative;
+  padding: 0.75rem 1.25rem;
+  margin-bottom: 1rem;
+  border: 1px solid transparent;
+  border-radius: 0.25rem;
+}
+
+.alert-info {
+  color: #31708f;
+  background-color: #d9edf7;
+  border-color: #bce8f1;
+}
+.alert-success {
+  color: #3c763d;
+  background-color: #dff0d8;
+  border-color: #d6e9c6;
+}
+.alert-warning {
+  color: #8a6d3b;
+  background-color: #fcf8e3;
+  border-color: #faebcc;
+}
+.alert-danger {
+  color: #721c24;
+  background-color: #f8d7da;
+  border-color: #f5c6cb;
+}
 </style>
+
+<script>
+import { mapActions, mapState } from "vuex";
+
+export default {
+  name: "home",
+  computed: {
+    ...mapState({
+      alert: state => state.alert
+    })
+  },
+  methods: {
+    ...mapActions("alert", {
+      clearAlert: "clear"
+    })
+  },
+  watch: {
+    // eslint-disable-next-line
+    $route(to, from) {
+      this.clearAlert();
+    }
+  }
+};
+</script>
